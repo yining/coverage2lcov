29248d61ac88bb110e0e34e3d8b9ce57a8117665
Zhang Yining <zhang.yining@gmail.com>
2022-10-28 01:21:25 +0800

    refactor: use `TryFrom` trait
    - replace `parse()` with `TryFrom::try_from`
    - update edition to `2021` (to stop complier/clippy whining)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::convert::TryFrom;
 use std::env;
 use std::fs::File;
-use std::io::{self, Lines, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Lines};
 use std::path::Path;
 
 mod lib;
@@ -18,9 +19,9 @@ fn main() {
 
     if let Ok(lines) = read_lines(coverage_file) {
         for line in lines.flatten() {
-            match FileCov::parse(&line) {
-                Some(fr) => println!("{}", fr),
-                None => continue,
+            match FileCov::try_from(line.as_str()) {
+                Ok(v) => println!("{}", v),
+                Err(_) => continue,
             }
         }
     } else {
@@ -29,7 +30,7 @@ fn main() {
     }
 }
 
-// TODO: a better read_lines fn
+// TODO: a better read_lines fn, and also support stdin
 fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
 where
     P: AsRef<Path>,
